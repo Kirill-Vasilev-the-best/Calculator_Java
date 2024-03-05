0ea2e96264086ae1c73b22545e48d26a89c1de98
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2024-03-05 21:19:31 +0300

    Итоговая работа калькулятор

---
+++ AddOperation.java
@@ -0,0 +1,8 @@
+package complexCalculator5;
+
+public class AddOperation implements ComplexOperation {
+    public ComplexNumber operate(ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.getReal() + b.getReal(), a.getImaginary() + b.getImaginary());
+    }
+}
+
